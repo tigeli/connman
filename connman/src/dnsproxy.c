a8364ad518b2d502d86a577ee5837ddceaf4e997
Pasi Sjöholm <pasi.sjoholm@jollamobile.com>
2014-07-04 00:11:44 +0300

    optimize4

--- connman/src/dnsproxy.c
+++ connman/src/dnsproxy.c
@@ -1,4 +1,4 @@
-/*
+r/*
  *
  *  Connection Manager
  *
@@ -2049,10 +2049,8 @@ static int forward_dns_reply(unsigned char *reply, int reply_len, int protocol,
 				reply = new_reply;
 				reply_len = header_len + uptr - uncompressed;
 
-				ptr = (char *)reply + header_len;
-
-				memmove(ptr + host_len + 1,
-					ptr + host_len + domain_len + 1,
+				memmove(uptr - uncompressed + host_len + 1,
+					uptr - uncompressed + host_len + domain_len + 1,
 					reply_len - header_len - domain_len);
 
 				reply_len = reply_len - domain_len;
