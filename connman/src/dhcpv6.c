7df68619a3a13f45340aa9fc9cd4572d54dc7ce6
Pasi Sjöholm <pasi.sjoholm@jollamobile.com>
2014-09-11 14:48:44 +0300

    Revert "dhcpv6: Reset last_request to current time with renew and rebind"
    This reverts commit 3da0742b76e6a2d65114633aa23750cf28c3a710.

--- connman/src/dhcpv6.c
+++ connman/src/dhcpv6.c
@@ -1136,7 +1136,6 @@ static void rebind_cb(GDHCPClient *dhcp_client, gpointer user_data)
 {
 	DBG("");
 
-	g_dhcpv6_client_reset_request(dhcp_client);
 	g_dhcpv6_client_clear_retransmit(dhcp_client);
 
 	re_cb(REQ_REBIND, dhcp_client, user_data);
@@ -1340,7 +1339,6 @@ static void renew_cb(GDHCPClient *dhcp_client, gpointer user_data)
 {
 	DBG("");
 
-	g_dhcpv6_client_reset_request(dhcp_client);
 	g_dhcpv6_client_clear_retransmit(dhcp_client);
 
 	re_cb(REQ_RENEW, dhcp_client, user_data);
